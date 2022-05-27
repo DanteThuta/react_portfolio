6675bced7566011afeee109335b458f8f7e47be5
Dante <dante@Dante-MacBook-Air.local>
2022-05-27 21:04:32 +0630

    Creating Experience Section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,84 @@
 import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>The Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End of FrontEnd */}
+        {/* Start of BackEnd */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div className="experience__detail-info">
+                <h4>Javascript</h4>
+                <p className="text-light">Intermediate</p>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
